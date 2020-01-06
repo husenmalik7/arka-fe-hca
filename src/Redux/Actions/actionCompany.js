0be3968de10e30e7a526f525321bfe836d7d510a
husen <husenmalik7@gmail.com>
2020-01-07 07:25:30 +0900

    qweqweasd

---
+++ src/Redux/Actions/actionCompany.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+// const URL = "http://localhost:9000/engineer";
+
+export const getCompanyById = (URL) => {
+    return {
+        type: "GET_COMPANY_BY_ID",
+        payload: axios.get(URL)
+    }
+}
