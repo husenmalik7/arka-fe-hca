baf49af1fbe73c5f89ed50b504dbb0e152505bb7
husenmalik7 <husenmalik7@gmail.com>
2021-11-24 10:52:59 +0700

    register page with regex handle data before submit

---
+++ src/helper/regex.js
@@ -0,0 +1,24 @@
+const lengthRegex = (string) => {
+  let pattern = /^.{3,100}$/;
+
+  return pattern.test(string);
+};
+
+const emailRegex = (string) => {
+  let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //https://www.w3resource.com/javascript/form/email-validation.php
+
+  return pattern.test(string);
+};
+
+const alertEmail = "please enter valid email";
+
+const passwordRegex = (string) => {
+  let pattern = /^[A-Za-z0-9]\w{7,14}$/; // https://www.w3resource.com/javascript/form/password-validation.php
+
+  return pattern.test(string);
+};
+
+const alertPassword =
+  "password must be between 8-15 characters, any word characters";
+
+export { lengthRegex, emailRegex, alertEmail, passwordRegex, alertPassword };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,10 +4,96 @@ import LeftSide from "../components/LeftSide";
 import Button from "../components/Button";
 
 import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { useState } from "react";
+
+import {
+  lengthRegex,
+  emailRegex,
+  alertEmail,
+  passwordRegex,
+  alertPassword,
+} from "../helper/regex";
 
 const Register = () => {
   const [role, setRole] = useState("company");
+
+  const [company, setCompany] = useState({
+    name: "",
+    email: "",
+    password: "",
+    location: "",
+    description: "",
+  });
+  const [engineer, setEngineer] = useState({
+    name: "",
+    email: "",
+    password: "",
+    location: "",
+  });
+
+  function handleRole(role) {
+    if (role === "company") {
+      setRole("company");
+
+      setEngineer({
+        name: "",
+        email: "",
+        password: "",
+        location: "",
+      });
+    } else {
+      setRole("engineer");
+
+      setCompany({
+        name: "",
+        email: "",
+        password: "",
+        location: "",
+        description: "",
+      });
+    }
+  }
+
+  function handleSubmit() {
+    if (role === "company") {
+      let isCompanyNameValid = lengthRegex(company.name);
+      if (!isCompanyNameValid)
+        return alert("company name must be 3 to 100 characters length");
+
+      let isCompanyEmailValid = emailRegex(company.email);
+      if (!isCompanyEmailValid) return alert(alertEmail);
+
+      let isCompanyPasswordValid = passwordRegex(company.password);
+      if (!isCompanyPasswordValid) return alert(alertPassword);
+
+      let isCompanyLocationValid = lengthRegex(company.location);
+      if (!isCompanyLocationValid)
+        return alert("company location must be 3 to 100 characters length");
+
+      let isCompanyDescriptionValid = lengthRegex(company.description);
+      if (!isCompanyDescriptionValid)
+        return alert("company description must be 3 to 100 characters length");
+
+      console.log({ company });
+    } else {
+      let isEngineerNameValid = lengthRegex(engineer.name);
+      if (!isEngineerNameValid)
+        return alert("engineer name must be 3 to 100 characters length");
+
+      let isEngineerEmailValid = emailRegex(engineer.email);
+      if (!isEngineerEmailValid) return alert(alertEmail);
+
+      let isEngineerPasswordValid = passwordRegex(engineer.password);
+      if (!isEngineerPasswordValid) return alert(alertPassword);
+
+      let isEngineerLocationValid = lengthRegex(engineer.location);
+      if (!isEngineerLocationValid)
+        return alert("engineer location must be 3 to 100 characters length");
+
+      console.log({ engineer });
+    }
+  }
 
   return (
     <div className="login-container">
@@ -28,7 +114,7 @@ const Register = () => {
                 type="radio"
                 id={`inline-radio-1`}
                 label="Company"
-                onClick={() => setRole("company")}
+                onClick={() => handleRole("company")}
                 defaultChecked
               />
               <Form.Check
@@ -36,52 +122,138 @@ const Register = () => {
                 type="radio"
                 id={`inline-radio-2`}
                 label="Engineer"
-                onClick={() => setRole("engineer")}
+                onClick={() => handleRole("engineer")}
               />
             </Form.Group>
 
             {role === "company" ? (
               <>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Company Name" />
+                <Form.Group className="mb-3" controlId="formCompanyName">
+                  <Form.Control
+                    type="name"
+                    placeholder="Company Name"
+                    value={company.name}
+                    onChange={(e) =>
+                      setCompany({
+                        ...company,
+                        name: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Company Email" />
+                <Form.Group className="mb-3" controlId="formCompanyEmail">
+                  <Form.Control
+                    type="email"
+                    placeholder="Company Email"
+                    value={company.email}
+                    onChange={(e) =>
+                      setCompany({
+                        ...company,
+                        email: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Control type="password" placeholder="Password" />
+                <Form.Group className="mb-3" controlId="formCompanyPassword">
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={company.password}
+                    onChange={(e) =>
+                      setCompany({
+                        ...company,
+                        password: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Location" />
+                <Form.Group className="mb-3" controlId="formCompanyLocation">
+                  <Form.Control
+                    type="name"
+                    placeholder="Location"
+                    value={company.location}
+                    onChange={(e) =>
+                      setCompany({
+                        ...company,
+                        location: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Short description" />
+                <Form.Group className="mb-3" controlId="formCompanyDescription">
+                  <Form.Control
+                    type="email"
+                    placeholder="Short description"
+                    value={company.description}
+                    onChange={(e) =>
+                      setCompany({
+                        ...company,
+                        description: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
               </>
             ) : (
               <>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Name" />
+                <Form.Group className="mb-3" controlId="formEngineerName">
+                  <Form.Control
+                    type="email"
+                    placeholder="Name"
+                    value={engineer.name}
+                    onChange={(e) =>
+                      setEngineer({
+                        ...engineer,
+                        name: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Email" />
+                <Form.Group className="mb-3" controlId="formEngineerEmail">
+                  <Form.Control
+                    type="email"
+                    placeholder="Email"
+                    value={engineer.email}
+                    onChange={(e) =>
+                      setEngineer({
+                        ...engineer,
+                        email: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Control type="password" placeholder="Password" />
+                <Form.Group className="mb-3" controlId="formEngineerPassword">
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={engineer.password}
+                    onChange={(e) =>
+                      setEngineer({
+                        ...engineer,
+                        password: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Location" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Date of birth" />
+                <Form.Group className="mb-3" controlId="formEngineerLocation">
+                  <Form.Control
+                    type="email"
+                    placeholder="Location"
+                    value={engineer.location}
+                    onChange={(e) =>
+                      setEngineer({
+                        ...engineer,
+                        location: e.target.value,
+                      })
+                    }
+                  />
                 </Form.Group>
               </>
             )}
@@ -89,11 +261,27 @@ const Register = () => {
         </div>
 
         <div className="button-container mt-5">
-          <Button buttonName="Submit" buttonType="1" />
+          <Button
+            action={() => handleSubmit()}
+            buttonName="Submit"
+            buttonType="1"
+          />
+        </div>
+
+        <div className="already-have-account" style={alreadyHaveAccountStyle}>
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            <p>already have an account?</p>
+          </Link>
         </div>
       </div>
     </div>
   );
+};
+
+let alreadyHaveAccountStyle = {
+  fontWeight: "bold",
+  justifyContent: "center",
+  display: "flex",
 };
 
 export default Register;
