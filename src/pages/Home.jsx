5a82efb0f0d44cccecd40edfbd0a85b67e4a566e
husenmalik7 <husenmalik7@gmail.com>
2021-12-13 14:49:39 +0700

    home | bugless pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "../styles/main.css";
 import "../styles/home.css";
 
 let card = (item, index) => (
-  <div className="card-custom">
+  <div className="card-custom" key={index}>
     <div className="card-empty">
       <img src="https://picsum.photos/200/300?random=2" alt="name" />
     </div>
     <div className="card-content">
-      <p className="name">{index}</p>
+      {/* <p className="name">{index}</p> */}
       <p className="position">{item}</p>
       <p className="email">email</p>
     </div>
@@ -17,55 +17,84 @@ let card = (item, index) => (
 );
 
 const Home = () => {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  for (let i = 0; i < 100; i++) {
-    arr.push(i);
-  }
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(4);
+  const [recordsPerPage] = useState(4);
+  const [listPost, setListPost] = useState([]);
 
-  //get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = arr.slice(indexOfFirstPost, indexOfLastPost);
+  function prevPage() {
+    if (currentPage > 1) {
+      console.log("prev");
 
-  console.log(indexOfLastPost);
-  console.log(indexOfFirstPost);
-  console.log(currentPosts);
-
-  // change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-  let pageNumbers = [];
-  let totalPosts = arr.length;
-
-  for (let i = 1; i < Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
+      setCurrentPage((prevCurrentPage) => prevCurrentPage - 1);
+      changePage(currentPage);
+    }
   }
 
-  console.log(pageNumbers);
+  function nextPage() {
+    if (currentPage < numPages()) {
+      console.log("next");
+      // currentPage++;
+      setCurrentPage(currentPage + 1);
+      changePage(currentPage);
+    }
+  }
+
+  function changePage(page) {
+    let list = [];
+    let pageSpan;
+
+    //validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    for (
+      let i = (page - 1) * recordsPerPage;
+      i < page * recordsPerPage && i < arr.length;
+      i++
+    ) {
+      list.push(arr[i]);
+      listPost.push(arr[i]);
+    }
+
+    pageSpan = page + "/" + numPages();
+
+    console.log({ list });
+    console.log({ pageSpan });
+    console.log({ listPost });
+
+    setListPost(list);
+  }
+
+  function numPages() {
+    return Math.ceil(arr.length / recordsPerPage);
+  }
+
+  useEffect(() => {
+    // initial state of page
+    changePage(currentPage);
+
+    //for prevent the state is left behind [1]
+  }, [currentPage]);
 
   return (
     <div className="main fd-col">
       <div className="nav"></div>
       <div className="content fd-row">
         <div className="inner-content">
-          {currentPosts.map((item, index) => card(item, index))}
+          {listPost.map((item, index) => card(item, index))}
         </div>
       </div>
       <div className="pagination">
-        {pageNumbers.map((number) => (
-          <li key={number}>
-            <a href="#" onClick={() => paginate(number)}>
-              {number}
-            </a>
-          </li>
-        ))}
+        <p onClick={() => prevPage()}>prev page</p>
+        <p onClick={() => nextPage()}>next page</p>
       </div>
     </div>
   );
 };
 
 export default Home;
+
+//[1] https://stackoverflow.com/questions/28773839/react-form-onchange-setstate-one-step-behind
+//[1] https://stackoverflow.com/questions/56247433/how-to-use-setstate-callback-on-react-hooks
