9abee66b55a2c094bff9b204194dca2a4d679ccf
husenmalik7 <husenmalik7@gmail.com>
2021-12-16 11:24:30 +0700

    home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,100 +1,129 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
+
+import baseUrl from "../helper/baseUrl";
 
 import "../styles/main.css";
 import "../styles/home.css";
 
-let card = (item, index) => (
-  <div className="card-custom" key={index}>
-    <div className="card-empty">
-      <img src="https://picsum.photos/200/300?random=2" alt="name" />
-    </div>
-    <div className="card-content">
-      {/* <p className="name">{index}</p> */}
-      <p className="position">{item}</p>
-      <p className="email">email</p>
-    </div>
-  </div>
-);
-
-const Home = () => {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+const Home = (props) => {
+  const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [recordsPerPage] = useState(4);
-  const [listPost, setListPost] = useState([]);
-
-  function prevPage() {
-    if (currentPage > 1) {
-      console.log("prev");
-
-      setCurrentPage((prevCurrentPage) => prevCurrentPage - 1);
-      changePage(currentPage);
-    }
-  }
-
-  function nextPage() {
-    if (currentPage < numPages()) {
-      console.log("next");
-      // currentPage++;
-      setCurrentPage(currentPage + 1);
-      changePage(currentPage);
-    }
-  }
-
-  function changePage(page) {
-    let list = [];
-    let pageSpan;
-
-    //validate page
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
-
-    for (
-      let i = (page - 1) * recordsPerPage;
-      i < page * recordsPerPage && i < arr.length;
-      i++
-    ) {
-      list.push(arr[i]);
-      listPost.push(arr[i]);
-    }
-
-    pageSpan = page + "/" + numPages();
-
-    console.log({ list });
-    console.log({ pageSpan });
-    console.log({ listPost });
-
-    setListPost(list);
-  }
-
-  function numPages() {
-    return Math.ceil(arr.length / recordsPerPage);
-  }
+  const [paginationObj, setPaginationObj] = useState({});
 
   useEffect(() => {
-    // initial state of page
-    changePage(currentPage);
+    fetchData();
+  }, []);
 
-    //for prevent the state is left behind [1]
-  }, [currentPage]);
+  let handleCard = (id) => {
+    props.history.push("/profile/" + id);
+  };
+
+  let fetchData = (page) => {
+    let paramsPage = new URLSearchParams(window.location.search).get("page");
+    if (paramsPage) page = paramsPage;
+    if (paramsPage === null) paramsPage = 1;
+
+    setCurrentPage(paramsPage);
+
+    let url = `${baseUrl}/engineer`;
+    axios
+      .get(url, { params: { page: page } })
+      .then((response) => {
+        let paginationObject = setPaginationObject(response);
+        setPaginationObj(paginationObject);
+        handleOverPage(page, paginationObject, props);
+
+        console.log({ paramsPage });
+        console.log({ paginationObject });
+
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    function setPaginationObject(item) {
+      let paginationObject = {};
+      paginationObject.page = item.data.page;
+      paginationObject.numOfPages = item.data.numOfPages;
+
+      return paginationObject;
+    }
+
+    function handleOverPage(page, paginationObject, props) {
+      if (page > paginationObject.page) {
+        props.history.push("/home?page=" + paginationObject.page);
+        setCurrentPage(paginationObject.page);
+      } else if (page < 1) {
+        props.history.push("/home?page=1");
+        setCurrentPage(1);
+      }
+    }
+  };
+
+  let card = (item, index) => (
+    <div
+      className="card-custom"
+      key={index}
+      onClick={() => handleCard(item.id)}
+    >
+      <div className="card-empty">
+        <img src={`https://picsum.photos/200/300?random=${index}`} alt="name" />
+      </div>
+      <div className="card-content">
+        <p className="name">{item.name}</p>
+        <p className="position">{item.position}</p>
+        <p className="email">{item.email}</p>
+      </div>
+    </div>
+  );
 
   return (
     <div className="main fd-col">
       <div className="nav"></div>
       <div className="content fd-row">
         <div className="inner-content">
-          {listPost.map((item, index) => card(item, index))}
+          {data.map((item, index) => card(item, index))}
         </div>
       </div>
       <div className="pagination">
-        <p onClick={() => prevPage()}>prev page</p>
-        <p onClick={() => nextPage()}>next page</p>
+        <div id="prev-button">
+          <div className="button-wrapper">
+            <a
+              className={
+                currentPage <= 1 ? `button type-disabled` : `button type-1`
+              }
+              href={
+                currentPage <= 1 ? `#` : `/home?page=${Number(currentPage) - 1}`
+              }
+            >
+              Previous
+            </a>
+          </div>
+        </div>
+
+        <div id="next-button">
+          <div className="button-wrapper">
+            <a
+              className={
+                currentPage >= paginationObj.numOfPages
+                  ? `button type-disabled`
+                  : `button type-1`
+              }
+              href={
+                currentPage >= paginationObj.numOfPages
+                  ? `#`
+                  : `/home?page=${Number(currentPage) + 1}`
+              }
+            >
+              Next
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
-
-//[1] https://stackoverflow.com/questions/28773839/react-form-onchange-setstate-one-step-behind
-//[1] https://stackoverflow.com/questions/56247433/how-to-use-setstate-callback-on-react-hooks
