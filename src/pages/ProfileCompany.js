a3fb44ffeebafb33f4bda06c3617a14cfc1813ed
husenmalik7 <husenmalik7@gmail.com>
2021-12-20 11:36:37 +0700

    profile company

--- src/pages/ProfileCompany.js
+++ src/pages/ProfileCompany.js
@@ -1,26 +1,33 @@
-/* eslint-disable */
-import React, { useEffect, useState } from "react";
-
-import "../styles/main.css";
-import { Spinner } from "react-bootstrap";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-const TestPage = (props) => {
-  const [testState, setTestState] = useState("udin");
-  const [data, setData] = useState([]);
+import "../styles/profile.css";
+import backLogo from "../assets/left-arrow.png";
+import Button from "../components/Button";
+import baseUrl from "../helper/baseUrl";
+
+const ProfileCompany = (props) => {
+  const [data, setData] = useState({});
 
   useEffect(() => {
     let localStorageData = {
       email: localStorage.getItem("email"),
       id: localStorage.getItem("id"),
       token: localStorage.getItem("token"),
-      name: localStorage.getItem("name"),
+      role: localStorage.getItem("role"),
     };
 
-    console.log(localStorageData);
+    if (localStorageData.role !== "company") {
+      alert("you must login as company to continue");
+      return props.history.push("/login");
+    }
 
-    // let url = `http://localhost:3001/company/${localStorageData.id}`;
-    let url = `http://localhost:3001/company/9`;
+    // console.log(localStorageData);
+
+    if (localStorageData.token === null) {
+      alert("you must login to continue");
+      return props.history.push("/login");
+    }
 
     let config = {
       headers: {
@@ -28,35 +35,78 @@ const TestPage = (props) => {
       },
     };
 
+    let id = localStorageData.id;
+    let url = `${baseUrl}/company/${id}`;
+
+    console.log(url);
     axios
       .get(url, config)
       .then((response) => {
-        console.log(response.data);
+        let resData = response.data.data[0];
+
+        let resMessage = response.data.msg;
+        if (resMessage.includes("expired")) {
+          alert("you must login to continue");
+          return props.history.push("/login");
+        }
+
+        setData({
+          name: resData.name,
+          position: resData.position,
+          description: resData.description,
+          email: resData.email,
+          location: resData.location,
+          skills: resData.skills,
+        });
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  function handleRemove() {
-    console.log("remove");
-    localStorage.removeItem("token");
-
-    props.history.push("/login");
+  function handleBack() {
+    props.history.push("/home");
   }
 
   return (
-    <div className="main">
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
+    <div className="profile">
+      <div className="upper-background"></div>
+      <div className="back-button">
+        <img
+          onClick={() => handleBack()}
+          src={backLogo}
+          alt="back-button"
+          width={"22px"}
+        />
+      </div>
+      <div className="left-box-container">
+        <div className="left-box-upperside">
+          <img
+            className="profile-image"
+            src="https://dummyimage.com/150x200/d1d1d1/616161"
+            alt="profile-pic"
+          />
+          <p className="bold-text">{data.name}</p>
+          <p>{data.email}</p>
+          <p>{data.location}</p>
+        </div>
 
-      <p>{testState}</p>
-      <p>company profile</p>
+        <div className="left-box-downside">
+          <Button buttonName="Edit" buttonType="1" />
+        </div>
+      </div>
+      <div className="right-box">
+        <div className="name-section">
+          <p className="bold-text larger">{data.name}</p>
+          <p className="bold-text">{data.position}</p>
+        </div>
 
-      <button onClick={() => handleRemove()}>asdasd</button>
+        <div className="description-section">
+          <p>{data.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default TestPage;
+export default ProfileCompany;
